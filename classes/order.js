55fc82d1caa5e184c514d33d86f91c42ef976012
Vincent Diribarne <vdiribarnepro@gmail.com>
2021-05-22 08:28:00 +0200

    Update

--- classes/order.js
+++
@@ -1,8 +0,0 @@
-module.exports = class Order {
-  constructor(livreur , xCord, yCord, timeLimit) {
-    this.livreur = livreur;
-    this.x = xCord;
-    this.y = yCord;
-    this.timeLimit = timeLimit
-  }
-};
