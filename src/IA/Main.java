2634f29061057251b0b3e7dad801cfd056e09273
Guymax <guymaxyt@gmail.com>
2021-05-22 08:43:02 +0200

    Added Base Classes

---
+++ src/IA/Main.java
@@ -0,0 +1,13 @@
+package IA;
+
+import network.Network;
+
+public class Main {
+	public static final Network socket = new Network();
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Joueur ia = new Joueur();
+	}
+
+}
