1a10c1f87b1d673d2ecf64f034d5cada3380f9c6
Baptiste Buvron <baptiste.buvron@gmail.com>
2021-05-22 09:32:46 +0200

    Début network

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import network.Network;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        Network network = new Network();
+    }
+}
--- src/network/Network.java
+++ src/network/Network.java
@@ -1,4 +1,52 @@
 package network;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class Network {
+
+    private Socket socket;
+
+    public Network() throws IOException {
+        this.socket = new Socket("127.0.0.1", 2121);
+        init();
+    }
+
+    public void init() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        String str = reader.readLine();
+        System.out.println(str);
+        if (str.equals("NAME")){
+            name("LaSekt");
+        }
+        waitStart();
+    }
+
+    public void waitStart() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        String str = reader.readLine();
+        System.out.println(str);
+    }
+
+
+    public void move(int nPlayer, String direction) throws IOException {
+        try (PrintWriter writer = new PrintWriter(socket.getOutputStream(), true)) {
+            writer.println("MOVE|"+nPlayer+"|"+direction);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String str = reader.readLine();
+        }
+    }
+
+    public void name(String name) throws IOException {
+        try (PrintWriter writer = new PrintWriter(socket.getOutputStream(), true)) {
+            writer.println(name);
+            writer.close();
+        }
+    }
+
+
+
 }
