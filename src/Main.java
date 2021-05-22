0befbb31c3760eaa0b6f6b16239d5982461cdf9d
Baptiste Buvron <baptiste.buvron@gmail.com>
2021-05-22 15:09:07 +0200

    Début du déplacement

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 
+import IA.Joueur;
 import map.Map;
 import network.Network;
 
@@ -6,12 +7,18 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class Main {
+    public static int idTeam;
+    public static Network network;
+
     public static void main(String args[]) throws IOException {
         Network network = new Network();
         Map map = new Map();
         network.init();
-        int idTeam = network.waitStart();
+        idTeam = network.waitStart();
+        Joueur ia = new Joueur();
         map.createMap(network.getMap());
         ArrayList<int[]> path = map.findPath(new Integer[]{0, 6},new Integer[]{6,9});
+
+
     }
 }
